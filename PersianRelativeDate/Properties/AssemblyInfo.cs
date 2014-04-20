662a43e97dfbce8e9ab35351e435bba5987ef54d
Mahdi Taghizadeh <i@mahdi.ws>
2014-04-20 14:42:19 +0430

    Update Nuget packages

--- PersianRelativeDate/Properties/AssemblyInfo.cs
+++ PersianRelativeDate/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PersianRelativeDate")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("This is a simple class inspired by the class written by Kelsey on StackOverflow (http://stackoverflow.com/a/3682673/27946) that returns a relative date similar to what you can see for example on Twitter. I just added a resource file and localized it for Persian language; no more!")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Dipudio")]
 [assembly: AssemblyProduct("PersianRelativeDate")]
